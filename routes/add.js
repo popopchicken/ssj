108a7e86125f55294143ecf4a72ac43c834807a9
Sachi Pitkin <spitkin@ucsd.edu>
2015-02-06 12:48:40 -0800

    pretty changes

--- routes/add.js
+++ routes/add.js
@@ -1,16 +1,17 @@
 var data = require("../data.json");
 
-exports.addFriend = function(req, res) {    
+exports.regProfile = function(req, res) {    
 	console.log("reach");
 	var newName = req.query.name;
-    var desc = req.query.description;
-    var img = 'http://lorempixel.com/400/400/people';
+    var newEmail = req.query.email;
+    var newPassword = req.query.password;
 
-    var  newFriend = {
+    var  newProfile = {
     				"name": newName,
-    				"description": desc,
-    				"imageURL": img
+    				"email": newEmail,
+    				"password": newPassword
+                    "picture": "http://lorempixel.com/500/500/people"    
     			};
-	data["friends"].push(newFriend);
+	data["profiles"].push(newProfile);
 	res.render('add');
  }
